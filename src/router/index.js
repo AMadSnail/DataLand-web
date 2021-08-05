0a40cd52439f84fc7b89baa85afa359d622a2ffe
AMadSnail <37287159+AMadSnail@users.noreply.github.com>
2021-08-05 10:43:28 +0800

    1
    create

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +11,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/header/index.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        // 当 /user/:id/profile 匹配成功，
+        // UserProfile 会被渲染在 User 的 <router-view> 中
+        path: '/soother',
+        component: () => import(/* webpackChunkName: "about" */ '../views/soulSoother/index.vue')
+      }
+    ]
   }
 ]
 
---
+++ src/views/soulSoother/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="soother-page">心灵鸡汤</div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scope>
+.soother-page{
+  margin: 0;
+}
+</style>
